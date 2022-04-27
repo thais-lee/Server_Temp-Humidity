508e041c0f5fd4dcbdac7d20d57f2c7e3d0d9c89
thais-lee <meoconlonton2001@gmail.com>
2022-04-27 17:00:36 +0700

    define axios request

---
+++ src/utils/fetchUtils.js
@@ -0,0 +1,30 @@
+const axios = require('axios');
+
+export const axiosMethod = {
+  GET: 'GET',
+  POST: 'POST',
+  PUT: 'PUT',
+  DELETE: 'DELETE',
+};
+
+export const axiosRequest = (
+  url,
+  method = axiosMethod.GET,
+  token = '',
+  params = null,
+  data = null,
+) => {
+  const axiosConfig = {
+    url,
+    method,
+    headers: {},
+    params,
+  };
+
+  if (token) {
+    axiosConfig.headers.Authorization = `Bearer ${token}`;
+  }
+
+  axiosConfig.data = data;
+  return axios(axiosConfig);
+};
