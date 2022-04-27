fb1c825afe7013b726338f0a92e74aec50393bdb
thais-lee <meoconlonton2001@gmail.com>
2022-04-27 16:59:47 +0700

    add constants of http

---
+++ src/config/constants/index.js
@@ -0,0 +1,18 @@
+export const HttpStatus = {
+    SUCCESS: 200,
+    BAD_REQUEST: 400,
+    UNAUTHORIZED: 401,
+    FORBIDDEN: 403,
+    NOT_FOUND: 404,
+    INTERNAL_SERVER_ERROR: 500,
+  };
+  
+  export const HttpCode = {
+    SUCCESS: 'Success',
+    BAD_REQUEST: 'BAD_REQUEST',
+    UNAUTHORIZED: 'UNAUTHORIZED',
+    FORBIDDEN: 'UNAUTHORIZED',
+    NOT_FOUND: 'NOT_FOUND',
+    INTERNAL_SERVER_ERROR: 'INTERNAL_SERVER_ERROR',
+  };
+  
